2dd004cd57a5906fcd51cf5a98e65d0c352a0cd0
Vampira <73371267+7GrandLittleBrother@users.noreply.github.com>
2022-05-31 14:36:33 +0800

    Update bedwars.lua

--- scripts/bedwars.lua
+++ scripts/bedwars.lua
@@ -1,3 +1,9 @@
--- when arceusx is back or kittenmill update
-
-print("sad")
+--[[ pls read
+  Credits to all the script i borrowed
+  Special Thanks to 7GrandDad for require() modules // i can't require a modules because i dont have pc
+  Credits 7GrandDad for AutoSprint // -- require() modules
+  Credits to YZHacker for AntiVoid Jump Help
+  All some missing credits will be go on credits tab
+  Special sorry for the intro because // maxlaser was a skid and not credting people
+  Special thanks to SVamp for making the script
+pls read]]
